0a96fa5896848d83a7fed79a00d98d029cf8e6e5
Felix Roos <flix91@gmail.com>
2025-09-06 00:42:36 +0200

    hotfix: comment out tauri stuff to fix osc

--- packages/osc/superdirtoutput.js
+++ packages/osc/superdirtoutput.js
@@ -1,8 +1,8 @@
-import { oscTriggerTauri } from '../desktopbridge/oscbridge.mjs';
-import { isTauri } from '../desktopbridge/utils.mjs';
+/* import { oscTriggerTauri } from '../desktopbridge/oscbridge.mjs';
+import { isTauri } from '../desktopbridge/utils.mjs'; */
 import { oscTrigger } from './osc.mjs';
 
-const trigger = isTauri() ? oscTriggerTauri : oscTrigger;
+const trigger = /* isTauri() ? oscTriggerTauri : */ oscTrigger;
 
 export const superdirtOutput = (hap, deadline, hapDuration, cps, targetTime) => {
   const currentTime = performance.now() / 1000;
